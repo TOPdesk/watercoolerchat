ea73b31e3e1405c7467a7e460bf6fd11b24ece14
István Rábel <i.rabel@topdesk.com>
2020-04-03 16:43:27 +0200

    Content type, encoding, return codes for assets. Caching in progress.

--- test/_server-test.js
+++ test/_server-test.js
@@ -14,13 +14,90 @@ test('Server can be started and stopped', t => {
 });
 
 test('Assets', sub => {
+	const headers = {host: 'localhost'};
+	const {respond} = new RequestHandler();
 	sub.test('Requesting dotfiles', t => {
-		const {respond} = new RequestHandler();
-		const request = {url: '/.hidden', headers: {host: 'localhost'}};
+		const request = {url: '/.hidden', headers};
 		const response = new MockResponse();
 		respond(request, response);
-		t.same([{code: 404, message: 'Not found'}], response.head(), 'Is a 404');
+		t.same(response.head(), [404, 'Not Found'], 'is a 404');
 		t.true(response.ended(), 'and closing response');
+		t.end();
+	});
+	sub.test('Breaking out of public', async t => {
+		let request;
+		let response;
+
+		request = {url: '../main.js', headers};
+		response = new MockResponse();
+		respond(request, response);
+		await response.promise;
+		t.same(response.head(), [404, 'Not Found'], '../main.js is a 404');
+		t.true(response.ended(), 'and closing response');
+
+		request = {url: '%2E%2E/main.js', headers};
+		response = new MockResponse();
+		respond(request, response);
+		await response.promise;
+		t.same(response.head(), [404, 'Not Found'], '%2E%2E/main.js is a 404');
+		t.true(response.ended(), 'and closing response');
+		t.end();
+	});
+	sub.test('Media Types', async t => {
+		let request;
+		let response;
+
+		request = {url: '/some.css', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.headers(), ['Content-Type', 'text/css; charset=utf-8'], 'css has text/css content type, and utf-8 encoding');
+		await response.promise;
+
+		request = {url: '/some.html', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.headers(), ['Content-Type', 'text/html; charset=utf-8'], 'html has text/html content type, and utf-8 encoding');
+		await response.promise;
+
+		request = {url: '/some.ico', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.headers(), ['Content-Type', 'image/x-icon'], 'ico has image/x-icon content type');
+		await response.promise;
+
+		request = {url: '/some.js', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.headers(), ['Content-Type', 'application/javascript; charset=utf-8'], 'js has text/html content type, and utf-8 encoding');
+		await response.promise;
+
+		request = {url: '/some.png', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.headers(), ['Content-Type', 'image/png'], 'png has image/png content type');
+		await response.promise;
+
+		request = {url: '/some.svg', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.headers(), ['Content-Type', 'image/svg+xml; charset=utf-8'], 'svg has image/svg+xml content type, and utf-8 encoding');
+		await response.promise;
+
+		request = {url: '/some.unknown', headers};
+		response = new MockResponse();
+		respond(request, response);
+		t.same(response.head(), [415, 'Unsupported Media Type'], 'unknown extension is a 415');
+		t.true(response.ended(), 'and closing response');
+
+		t.end();
+	});
+	sub.test('Caching', async t => {
+		const request = {url: '/favicon.ico', headers: Object.assign(headers, {'if-modified-since': '3000-01-01T01:00:00.000Z'})};
+		const response = new MockResponse();
+		respond(request, response);
+		await response.promise;
+		t.same(response.head(), [304, 'Not Modified'], 'if not modified sine, it is a 304');
+		// TODO: cache control, last modified in response
 		t.end();
 	});
 	sub.end();
@@ -28,13 +105,28 @@ test('Assets', sub => {
 
 function MockResponse() {
 	const head = [];
+	const headers = [];
 	let ended = false;
+	let _resolve = null;
+	const promise = new Promise(resolve => {
+		_resolve = resolve;
+	});
 	return {
-		head: () => head,
+		head: () => Object.freeze(head),
 		ended: () => ended,
-		writeHead: (code, message) => head.push({code, message}),
+		writeHead: (code, message) => {
+			head.push(code);
+			head.push(message);
+		},
 		end: () => {
 			ended = true;
-		}
+			_resolve();
+		},
+		setHeader: (header, value) => {
+			headers.push(header);
+			headers.push(value);
+		},
+		headers: () => Object.freeze(headers),
+		promise
 	};
 }
