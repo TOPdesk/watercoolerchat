0cc7fa605f200bd149077de04f8e08100424d9c3
István Rábel <i.rabel@topdesk.com>
2020-04-06 16:20:21 +0200

    Cache Control, If-Modified-Since, Last-Modified, ETag

--- test/_server-test.js
+++ test/_server-test.js
@@ -1,7 +1,11 @@
 import tap from 'tap';
+import {promises as fsPromises} from 'fs';
+import {Writable} from 'stream';
+import {createHash} from 'crypto';
 import {Server, RequestHandler} from '../lib/_server.js';
 
 const {test} = tap;
+const {stat, readFile} = fsPromises;
 
 test('Server can be started and stopped', t => {
 	t.plan(2);
@@ -21,7 +25,6 @@ test('Assets', sub => {
 		const response = new MockResponse();
 		respond(request, response);
 		t.same(response.head(), [404, 'Not Found'], 'is a 404');
-		t.true(response.ended(), 'and closing response');
 		t.end();
 	});
 	sub.test('Breaking out of public', async t => {
@@ -31,16 +34,14 @@ test('Assets', sub => {
 		request = {url: '../main.js', headers};
 		response = new MockResponse();
 		respond(request, response);
-		await response.promise;
+		await response.finished;
 		t.same(response.head(), [404, 'Not Found'], '../main.js is a 404');
-		t.true(response.ended(), 'and closing response');
 
 		request = {url: '%2E%2E/main.js', headers};
 		response = new MockResponse();
 		respond(request, response);
-		await response.promise;
+		await response.finished;
 		t.same(response.head(), [404, 'Not Found'], '%2E%2E/main.js is a 404');
-		t.true(response.ended(), 'and closing response');
 		t.end();
 	});
 	sub.test('Media Types', async t => {
@@ -50,83 +51,109 @@ test('Assets', sub => {
 		request = {url: '/some.css', headers};
 		response = new MockResponse();
 		respond(request, response);
-		t.same(response.headers(), ['Content-Type', 'text/css; charset=utf-8'], 'css has text/css content type, and utf-8 encoding');
-		await response.promise;
+		await response.finished;
+		t.same(response.headers().slice(-2), ['Content-Type', 'text/css; charset=utf-8'], 'css has text/css content type, and utf-8 encoding');
 
 		request = {url: '/some.html', headers};
 		response = new MockResponse();
 		respond(request, response);
-		t.same(response.headers(), ['Content-Type', 'text/html; charset=utf-8'], 'html has text/html content type, and utf-8 encoding');
-		await response.promise;
+		await response.finished;
+		t.same(response.headers().slice(-2), ['Content-Type', 'text/html; charset=utf-8'], 'html has text/html content type, and utf-8 encoding');
 
 		request = {url: '/some.ico', headers};
 		response = new MockResponse();
 		respond(request, response);
-		t.same(response.headers(), ['Content-Type', 'image/x-icon'], 'ico has image/x-icon content type');
-		await response.promise;
+		await response.finished;
+		t.same(response.headers().slice(-2), ['Content-Type', 'image/x-icon'], 'ico has image/x-icon content type');
 
 		request = {url: '/some.js', headers};
 		response = new MockResponse();
 		respond(request, response);
-		t.same(response.headers(), ['Content-Type', 'application/javascript; charset=utf-8'], 'js has text/html content type, and utf-8 encoding');
-		await response.promise;
+		await response.finished;
+		t.same(response.headers().slice(-2), ['Content-Type', 'application/javascript; charset=utf-8'], 'js has text/html content type, and utf-8 encoding');
 
 		request = {url: '/some.png', headers};
 		response = new MockResponse();
 		respond(request, response);
-		t.same(response.headers(), ['Content-Type', 'image/png'], 'png has image/png content type');
-		await response.promise;
+		await response.finished;
+		t.same(response.headers().slice(-2), ['Content-Type', 'image/png'], 'png has image/png content type');
 
 		request = {url: '/some.svg', headers};
 		response = new MockResponse();
 		respond(request, response);
-		t.same(response.headers(), ['Content-Type', 'image/svg+xml; charset=utf-8'], 'svg has image/svg+xml content type, and utf-8 encoding');
-		await response.promise;
+		await response.finished;
+		t.same(response.headers().slice(-2), ['Content-Type', 'image/svg+xml; charset=utf-8'], 'svg has image/svg+xml content type, and utf-8 encoding');
 
 		request = {url: '/some.unknown', headers};
 		response = new MockResponse();
 		respond(request, response);
+		await response.finished;
 		t.same(response.head(), [415, 'Unsupported Media Type'], 'unknown extension is a 415');
-		t.true(response.ended(), 'and closing response');
 
 		t.end();
 	});
 	sub.test('Caching', async t => {
-		const request = {url: '/favicon.ico', headers: Object.assign(headers, {'if-modified-since': '3000-01-01T01:00:00.000Z'})};
-		const response = new MockResponse();
+		let request = {url: '/favicon.ico', headers: {'if-modified-since': '3000-01-01T01:00:00.000Z', ...headers}};
+		let response = new MockResponse();
 		respond(request, response);
-		await response.promise;
+		await response.finished;
 		t.same(response.head(), [304, 'Not Modified'], 'if not modified sine, it is a 304');
-		// TODO: cache control, last modified in response
+
+		const {mtime: faviconMtime} = await stat('public/favicon.ico');
+		const faviconBuffer = await readFile('public/favicon.ico');
+		const hash = createHash('md5');
+		hash.setEncoding('hex');
+		hash.end(faviconBuffer);
+		const faviconHash = hash.read();
+
+		request = {url: '/favicon.ico', headers};
+		response = new MockResponse();
+		respond(request, response);
+		await response.finished;
+		t.same(response.headers().slice(0, 2), ['Last-Modified', faviconMtime], 'response has Last Modified header set');
+		t.same(response.headers().slice(2, 4), ['ETag', faviconHash], 'response has Etag header set');
+		t.same(response.headers().slice(4, 6), ['Cache-Control', 'max-age=0, private'], 'response has Cache Control header set');
+		t.same(response.buffer(), faviconBuffer, 'returns content when no If-Modified-Since heder was sent');
+
+		request = {url: '/favicon.ico', headers: {'if-modified-since': '2000-01-01T01:00:00.000Z', ...headers}};
+		response = new MockResponse();
+		respond(request, response);
+		await response.finished;
+		t.same(response.buffer(), faviconBuffer, 'returns content when modified since');
 		t.end();
 	});
 	sub.end();
 });
 
 function MockResponse() {
+	const stream = new Writable();
 	const head = [];
 	const headers = [];
-	let ended = false;
+	let buffer = Buffer.from([]);
 	let _resolve = null;
-	const promise = new Promise(resolve => {
+
+	stream.finished = new Promise(resolve => {
 		_resolve = resolve;
 	});
-	return {
-		head: () => Object.freeze(head),
-		ended: () => ended,
-		writeHead: (code, message) => {
-			head.push(code);
-			head.push(message);
-		},
-		end: () => {
-			ended = true;
-			_resolve();
-		},
-		setHeader: (header, value) => {
-			headers.push(header);
-			headers.push(value);
-		},
-		headers: () => Object.freeze(headers),
-		promise
+	stream.head = () => Object.freeze(head);
+	stream.headers = () => Object.freeze(headers);
+	stream.write = chunk => {
+		buffer = Buffer.concat([buffer, chunk]);
 	};
+
+	stream._end = stream.end;
+	stream.end = (chunk, encoding) => stream._end(chunk, encoding, _resolve);
+	stream.buffer = () => buffer;
+
+	stream.setHeader = (header, value) => {
+		headers.push(header);
+		headers.push(value);
+	};
+
+	stream.writeHead = (code, message) => {
+		head.push(code);
+		head.push(message);
+	};
+
+	return stream;
 }
