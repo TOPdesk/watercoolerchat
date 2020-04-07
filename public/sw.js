3f98a1290819aefc1b20bfb6ae56e5011eee6f2e
Joep Weijers <j.weijers@topdesk.com>
2020-04-07 18:57:15 +0200

    Make the notification clickable to open the watercoolerchat

--- public/sw.js
+++ public/sw.js
@@ -3,10 +3,26 @@ self.addEventListener('push', event => {
 	console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
 	const title = 'Watercooler chat';
+	const {message, companyName} = JSON.parse(event.data.text());
 	const options = {
-		body: event.data.text(),
+		body: message,
+		data: {
+			companyName
+		},
 		icon: 'watercoolerchat.svg'
 	};
 
 	event.waitUntil(self.registration.showNotification(title, options));
+});
+
+self.addEventListener('notificationclick', event => {
+	const {notification, action} = event;
+	const {companyName} = notification.data;
+
+	if (action === 'close') {
+		notification.close();
+	} else {
+		clients.openWindow(`https://have-a.watercoolerchat.online/at/${companyName}`); // eslint-disable-line no-undef
+		notification.close();
+	}
 });
